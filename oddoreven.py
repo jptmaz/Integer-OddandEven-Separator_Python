29faf94476b2a90ac48d8eb3348201edee0abb2f
Jasmin Paige <jaspagmaz98@gmail.com>
2023-04-27 10:01:29 +0800

    Guide for problem #1

---
+++ oddoreven.py
@@ -0,0 +1,8 @@
+# Write a Python program that reads a text file named numbers.txt that contains 20 integers.
+# The program will create two other text file; the first text file will be named even.txt that will contains all even numbers extracted from the numbers.txt. 
+# The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.
+    
+# Open the text file containing the 20 integers and have the program read it.
+# Then create two text files, one for even numbers, the other one for odd numbers.
+# If the number is even, write to the even numbers text file
+# If the number is odd, write to the odd numbers text file.
