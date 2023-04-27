7592db7e43cf5ee2a795a8a94e5f7dc19d7ad600
Jasmin Paige <jaspagmaz98@gmail.com>
2023-04-27 10:15:34 +0800

    Read the text file

--- oddoreven.py
+++ oddoreven.py
@@ -3,12 +3,15 @@
 # The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.
     
 # Open the text file containing the 20 integers and have the program read it.
+
 import sys
 print(sys.path)
 
 with open("C:/Users/HomePC/OOP_SourceCode/OddandEven/numbers.txt") as int_file:
-    print (int_file)
-    
+    integers = int_file.readlines()
+    print (integers)
+
 # Then create two text files, one for even numbers, the other one for odd numbers.
-# If the number is even, write to the even numbers text file
-# If the number is odd, write to the odd numbers text file.
+# Identify the numbers if they are odd or even
+    # If the number is even, write to the even numbers text file
+    # If the number is odd, write to the odd numbers text file.
